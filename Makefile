2c8b3f1e28cd5887f14fd442349dfb4f2684f063
smith <nayache@student.42.fr>
2021-06-22 16:55:35 +0200

    clean_historyyy

--- Makefile
+++ Makefile
@@ -31,6 +31,7 @@ SRCS = src/start/main.c \
        src/cmd/cd_com.c \
        src/cmd/echo_com.c \
        src/cmd/export_com.c \
+	   src/cmd/history_com.c \
        src/utils/alloc_utils.c \
        src/utils/close_utils.c \
        src/utils/signal_handler.c \
--- src/cmd/history_com.c
+++ src/cmd/history_com.c
@@ -6,7 +6,7 @@
 /*   By: ssar <ssar@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 12:07:04 by ssar              #+#    #+#             */
-/*   Updated: 2021/06/22 14:31:35 by ssar             ###   ########.fr       */
+/*   Updated: 2021/06/22 16:40:51 by nayache          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	check_nb(char *str)
 	int j;
 
 	j = 0;
-	if (str[j][0] == '-')
+	if (str[0] == '-')
 		return (-1);
 	while (str[j])
 	{
@@ -35,10 +35,10 @@ int	check_nb(char *str)
 
 void	delete_row(t_sh *sh, char **lst, int *i)
 {
-	while	(check_nb(lst[*i]) == 1)
-	{
+	//while	(check_nb(lst[*i]) == 1)
+	//{
 	
-	}
+//	}
 }
 
 void	history_command(t_sh *sh, char **lst_arg, t_actual *actu)
@@ -46,16 +46,17 @@ void	history_command(t_sh *sh, char **lst_arg, t_actual *actu)
 	int i;
 
 	i = 1;
-	if (actu->arg_command == 1)
-		print_history();
+	if (lst_arg[1] == NULL)
+		return (print_history(sh->history));
 	while (lst_arg[i])
 	{
-		if (ft_comp(lst_arg[i], "-a") == 0)
-		else if (ft_comp(lst_arg[i], "-c") == 0)
-		else if (ft_comp(lst_arg[i], "-d") == 0)
-			delete_row(sh, lst_arg, &i);
-		else if (ft_comp(lst_arg[i], "-r") == 0)
-		else if (ft_comp(lst_arg[i], "-w") == 0)
+		//if (ft_comp(lst_arg[i], "-a") == 0)
+		if (ft_comp(lst_arg[i], "-c") == 0)
+			return (clean_history(sh->history));
+		if (ft_comp(lst_arg[i], "-d") == 0)
+			return (delete_row(sh, lst_arg, &i));
+	//	else if (ft_comp(lst_arg[i], "-r") == 0)
+	//	else if (ft_comp(lst_arg[i], "-w") == 0)
 		else
 		{
 			ft_error(sh, "invalid option", "history", lst_arg[i]);
